d3b39160c610edd1b40be93d615f06e9af6a1842
Fredeau123 <76597292+Fredeau123@users.noreply.github.com>
2024-01-29 17:18:12 +0100

    react working

---
+++ my-app/src/app/tictactoe.js
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import "./styles.css";
+
+import App from "./app";
+
+function TicTacToe() {
+    return (
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
+}
+
+export default TicTacToe;
